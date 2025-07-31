ca948aa47df728b7b461df7881d819d5b58f9cc7
sofu7mars <vko300748@gmail.com>
2025-07-31 09:39:10 +0200

    min_rev_arr

---
+++ min_reverse_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <set>
+#include <map>
+#include <list>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+int main() {
+
+    int arr[] = {4, 5, 6, 2, 3};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int l = 0, r = size - 1;
+    int m = (int) ((l + r) / 2);
+    cout << ((3 + 4) / 2) << endl;
+    
+    while (l < r) {
+        if (arr[l] > arr[r]) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+        m = (int) ((l + r) / 2); 
+    } 
+
+    cout << arr[l] << endl;
+
+    return 0;
+}
